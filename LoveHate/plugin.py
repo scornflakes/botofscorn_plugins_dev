52b6364af5db5ad01cd5f6a2032b42fbfbb19f2e
ntkurapati <ntkurapati@users.noreply.github.com>
2015-02-15 16:53:24 -0500

    unpingatize lovehate

--- LoveHate/plugin.py
+++ LoveHate/plugin.py
@@ -180,7 +180,7 @@ class LoveHate(callbacks.Plugin):
                 if len(results[key]) > 1:
                     results2 = []
                     for result in results[key]:
-                        results2 += _unpingatize(result, irc.state.channels[channel].nicks)
+                        results2 += _unpingatize(result, irc.state.channels[msg.args[0]].nicks)
                     users = ', '.join(results2[0:-1]) + ' and ' + results2[-1]
                     verb = key
                 else:
