fd2635490a1ba49749f3f1cc87f47fc88cc4033b
scornflakes <scornflakes@users.noreply.github.com>
2015-08-16 11:46:23 -0500

    op_no
    added op_no

--- MNFHRules/plugin.py
+++ MNFHRules/plugin.py
@@ -167,6 +167,16 @@ class MNFHRules(callbacks.Plugin):
         irc.reply("\xe2\x97\x94\xcc\xaf\xe2\x97\x94 %s" % user_name)
     eyeroll = wrap(eyeroll, [optional('text')])
 
+    @internationalizeDocstring
+    def op_no(self, irc, msg, args, user_name):
+        """ roll your eyes at person
+        """
+        if not user_name:
+            user_name = ""
+            if msg.nick in self.registryValue('ops', current_channel).split(','):
+                irc.reply("Please do not do this, %s" % user_name)
+    op_no = wrap(op_no, [optional('text')])
+
 
     def testops(self, irc, msg, args):
         """ test op command
